e9c4ee83c18fd6e2cd61d184d836513cbac89a7e
steinikarls <steinikarls@live.com>
2016-10-23 14:53:47 +0000

    Create header.php

---
+++ header.php
@@ -0,0 +1,25 @@
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="http://tsuts.tskoli.is/2T/1810943469/GSOVERK4/">Start Bootstrap</a>
+            </div>         
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="http://tsuts.tskoli.is/2T/1810943469/GSOVERK4/form.php">Form</a>
+                    </li>
+                    <li>
+                        <a href="#">Services</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
