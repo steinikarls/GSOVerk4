3079bf2e02e2cbec8f0dae11e0c7655e13abe149
steinikarls <steinikarls@live.com>
2016-10-23 14:57:01 +0000

    Create random_image.php

---
+++ random_image.php
@@ -0,0 +1,14 @@
+<?php 
+$images = [
+['file' => 'myndir/image-1.jpg',
+ 'caption' => 'Fjallagarður'],
+['file' => 'myndir/image-2.jpg',
+ 'caption' => 'trégarður'],
+['file' => 'myndir/image-3.jpg', 
+'caption' => 'á í skógi'],
+['file' => 'myndir/image-4.jpg',
+ 'caption' => 'Frumskógur']
+];
+shuffle($images);
+
+?>
